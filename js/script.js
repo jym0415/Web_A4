0afbc483104033affa701879aff365d5a07df674
jym0415 <jym9504@gmail.com>
2022-03-26 17:47:30 +0900

    유진건설

--- js/script.js
+++ js/script.js
@@ -1,31 +1,21 @@
-// $(document).ready(function(){
-//     var count = 0 ;
-//     var total = $("#slider li").length;
-
-// setInterval(function(){
-// count++
-// count %= total
-
-//     $("#slider li").eq(count).addClass("show").siblings().removeClass("show")
-
-// },3000)
-
-
-
-// })
-
 $(document).ready(function(){
     var count = 0;
-    var classNm = "show"
-    var total = $("#slider li").length; 
-    setInterval(function(){
-      count++
-      count %= total 
-
-  
-      $("#slider li").eq(count).addClass(classNm).siblings().removeClass(classNm)
-
-    }, 3000);
+    var total = $("#slider li").length;
 
 
-  }) 
+setInterval(function(){
+    count ++
+    count %= total
+    $("#slider li").eq(count).addClass("show").siblings().removeClass("show");
+},3000);
+
+$("#trigger").click(function(){
+    $('.popup').removeClass('d-none');
+});
+$(".popup button").click(function(){
+    $(".popup").addClass('d-none');
+});
+
+
+
+})
